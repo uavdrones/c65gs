54e709bd96aca777383283096c82a38986240dad
gardners <paul@servalproject.org>
2014-02-11 13:30:15 +1030

    add alternate sd card driver implementation. Issue #24

--- sdcardio.vhdl
+++ sdcardio.vhdl
@@ -5,8 +5,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 use Std.TextIO.all;
 use work.debugtools.all;
-use work.CommonPckg.all;
-use work.SdCardPckg.all;
 
 entity sdcardio is
   port (
@@ -33,36 +31,6 @@ entity sdcardio is
 end sdcardio;
 
 architecture behavioural of sdcardio is
-
-    component SdCardCtrl is
-    generic (
-      FREQ_G          : real       := 100.0;  -- Master clock frequency (MHz).
-      INIT_SPI_FREQ_G : real       := 0.4;  -- Slow SPI clock freq. during initialization (MHz).
-      SPI_FREQ_G      : real       := 25.0;  -- Operational SPI freq. to the SD card (MHz).
-      BLOCK_SIZE_G    : natural    := 512;  -- Number of bytes in an SD card block or sector.
-      CARD_TYPE_G     : CardType_t := SD_CARD_E  -- Type of SD card connected to this controller.
-      );
-    port (
-      -- Host-side interface signals.
-      clk_i      : in  std_logic;       -- Master clock.
-      reset_i    : in  std_logic                     := NO;  -- active-high, synchronous  reset.
-      rd_i       : in  std_logic                     := NO;  -- active-high read block request.
-      wr_i       : in  std_logic                     := NO;  -- active-high write block request.
-      continue_i : in  std_logic                     := NO;  -- If true, inc address and continue R/W.
-      addr_i     : in  std_logic_vector(31 downto 0) := x"00000000";  -- Block address.
-      data_i     : in  std_logic_vector(7 downto 0)  := x"00";  -- Data to write to block.
-      data_o     : out std_logic_vector(7 downto 0)  := x"00";  -- Data read from block.
-      busy_o     : out std_logic;  -- High when controller is busy performing some operation.
-      hndShk_i   : in  std_logic;  -- High when host has data to give or has taken data.
-      hndShk_o   : out std_logic;  -- High when controller has taken data or has data to give.
-      error_o    : out std_logic_vector(15 downto 0) := (others => NO);
-      -- I/O signals to the external SD card.
-      cs_bo      : out std_logic                     := HI;  -- Active-low chip-select.
-      sclk_o     : out std_logic                     := LO;  -- Serial clock to SD card.
-      mosi_o     : out std_logic                     := HI;  -- Serial data output to SD card.
-      miso_i     : in  std_logic                     := ZERO  -- Serial data input from SD card.
-      );
-  end component;
   
   component sd_controller is
     port (
@@ -81,8 +49,8 @@ architecture behavioural of sdcardio is
         );
   end component;
     
-  signal rd_is           : std_logic := '0';
-  signal wr_is           : std_logic := '0';
+  signal sd_doread       : std_logic := '0';
+  signal sd_dowrite      : std_logic := '0';
   signal hndShk_is       : std_logic := '0';
   signal hndShk_os       : std_logic;
   
@@ -126,33 +94,24 @@ begin  -- behavioural
   --**********************************************************************
   -- SD card controller module.
   --**********************************************************************
-  u3 : SdCardCtrl
-    generic map (
-      -- XXX Fix FREQ_G to be based off pixelclock, not variable cpuclock
-      FREQ_G => 48.0,                   -- CPU at 48MHz
-      CARD_TYPE_G => SD_CARD_E  -- Type of SD card connected to this c
-      )
+  
+  sd0: sd_controller 
     port map (
-      clk_i      => clock,
-      -- Internal control signals
-      reset_i    => sd_reset,
-      rd_i       => rd_is,
-      wr_i       => wr_is,
-      continue_i => '0',
-      addr_i     => sd_sector,
-      data_i     => sd_wdata,
-      data_o     => sd_rdata,
-      busy_o     => sd_busy,
-      hndShk_i   => hndShk_is,
-      hndShk_o   => hndShk_os,
-      error_o    => sd_errorcode,
-      -- External signals to SD card slot
-      cs_bo      => cs_bo,
-      sclk_o     => sclk_o,
-      mosi_o     => mosi_o,
-      miso_i     => miso_i
-      );
+	cs => cs_bo,
+	mosi => mosi_o,
+	miso => miso_i,
+	sclk => sclk_o,
 
+	rd =>  sd_doread,
+	wr =>  sd_dowrite,
+	dm_in => '1',	-- data mode, 0 = write continuously, 1 = write single block
+	reset => sd_reset,
+	din => sd_wdata,
+	dout => sd_rdata,
+	clk => clock	-- twice the SPI clk.  XXX Cannot exceed 50MHz
+);
+
+  
   -- XXX also implement F1011 floppy controller emulation.
   process (clock,fastio_addr,fastio_wdata,sector_buffer_mapped,sdio_busy,
            sd_reset,fastio_read,sd_sector,fastio_write,
@@ -416,15 +375,16 @@ begin  -- behavioural
           when ReadSector =>
             -- Begin reading a sector into the buffer
             if sd_busy='0' then
-              rd_is <= '0';
+              sd_doread <= '1';
               sd_state <= ReadingSector;
               sdio_busy <= '1';
               sector_offset <= (others => '0');
             else
-              rd_is <= '1';
+              sd_doread <= '0';
             end if;
           when ReadingSector =>
             if hndShk_os='1' then
+              sd_doread <= '0';
               -- A byte is ready to read, so store it
               -- XXX DOES contribute to ISE thinking that sector_buffer() is
               -- dual port
@@ -452,15 +412,16 @@ begin  -- behavioural
           when WriteSector =>
             -- Begin writing a sector into the buffer
             if sd_busy='0' then
-              wr_is <= '0';
+              sd_dowrite <= '1';
               sdio_busy <= '1';
               sd_state <= WritingSector;
               sector_offset <= (others => '0');
             else
-              wr_is <= '1';
+              sd_dowrite <= '0';
             end if;
           when WritingSector =>
             if hndShk_os='1' then
+              sd_dowrite <= '0';
               -- A byte is ready to read, so store it
               -- XXX DOES contribute to ISE thinking sector_buffer is dual port
               -- Commenting out the read from sector_buffer here fixes the problem.
